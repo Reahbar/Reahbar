60a9cd64965eafe87db97f38e22bbc2ac0d1f61e
Reahbar <132294046+Reahbar@users.noreply.github.com>
2023-05-07 07:00:09 +0530

    Add files via upload

---
+++ Looping Challenge.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: MIT 
+
+//Looping Statements and Functions
+//Looping Challenge
+//Looping in Action
+//Let's put our new knowledge into action!.
+
+//We'll do a little lab where you will do a stress test a for loop. This is not graded but rather a way for you to practice ahead of our final challenge. Use this to try loops out and share what you learn. To get an easier result, you may want to reduce your gas limit from 3 million down to something like 300k in the deploy section of remix.
+
+//For this challenge, do a simple operation, and run it in 1000 loops.
+
+//Alternatively, try to do a complex operation and run it in 100 loops.
+
+//Does the transaction throw an error? Why?
+
+//Share your answer in the Discord channel
+pragma solidity ^0.8.0;
+contract LoopingChallenge {
+
+    function Operation1000 () external pure 
+    {
+        
+        for (uint i = 0; i < 1000; i++) {
+            // code <- means code will run which is i == 2;
+        if (i == 3) {
+            continue;
+        }
+         // code <- means code will run which is i == 4;
+
+        if (i == 5) {
+            continue;
+        }
+         // code <- means code will run which is i == 6;
+
+         if (i == 7) {
+             continue;
+         }
+         // code <- means code will run which is i == 8;
+
+         if (i == 9) {
+             continue;
+         }
+         // code <- means code will run which is i == 10;
+
+         if (i == 11) {
+             continue;
+         }
+         // code <- means code will run which is i == 12;
+
+         if (i == 13) {
+             continue;
+         }
+         // code <- means code will run which is i == 14;
+
+         if (i == 15) {
+             continue;
+         }
+         // code <- means code will run which is i == 16;
+
+         if (i == 17) {
+             continue;
+         }
+         // code <- means code will run which is i == 18;
+
+         if (i == 19) {
+             continue;
+         }
+         // code <- means code will run which is i == 20;
+
+         if (i == 22) {
+             continue;
+         }
+         // code <- means code will run which is i == 23;
+
+         if (i == 24) {
+             continue;
+         }
+         // code <- means code will run which is i == 25;
+         
+         if (i == 26) {
+             continue;
+         }
+         // code <- means code will run which is i == 27;
+
+         if (i == 28) {
+             continue;
+         }
+         // code <- means code will run which is i == 29;
+
+         if (i == 30) {
+             continue;
+         }
+         // code <- means code will run which is i == 31;
+
+         if (i == 32) {
+             continue;
+         }
+         // code <- means code will run which is i == 33 ;
+
+         if (i == 34) {
+             continue;
+         }
+         // code <- means code will run which is i == 35;
+
+         if (i == 36) {
+             continue;
+         }
+         // code <- means code will run which is i == 37;
+
+         if (i == 38) {
+             continue;
+         }
+         // code <- means code will run which is i == 39;
+
+         if (i == 40) {
+             continue;
+         }
+         // code <- means code will run which is i == 41;
+
+         if (i == 42) {
+             continue;
+         }
+         // code <- means code will run which is i == 43;
+
+         if (i == 44) {
+             continue;
+         }
+         // code <- means code will run which is i == 45;
+
+         if (i == 46) {
+             continue;
+         }
+         // code <- means code will run which is i == 47;
+
+         if (i == 48) {
+             continue;
+         }
+         // code <- means code will run which is i == 49;
+
+         if (i == 50) {
+             continue;
+         }
+         // code <- means code will run which is i == 51;
+
+         if (i == 52) {
+             continue;
+         }
+         // code <- means code will run which is i == 53;
+
+         if (i == 54) {
+             continue;
+         }
+         // code <- means code will run which is i == 55;
+
+         if (i == 56) {
+             continue;
+         }
+         // code <- means code will run which is i == 57;
+
+         if (i == 58) {
+             continue;
+         }
+         // code <- means code will run which is i == 59;
+
+         if (i == 60) {
+             continue;
+         }
+         // code <- means code will run which is i == 61;
+
+         if (i == 62) {
+             continue;
+         }
+         // code <- means code will run which is i == 63;
+
+         if (i == 64) {
+             continue;
+         }
+         // code <- means code will run which is i == 65;
+
+         if (i == 66) {
+             continue;
+         }
+         // code <- means code will run which is i == 67;
+
+         if (i == 68) {
+             continue;
+         }
+         // code <- means code will run which is i == 69;
+
+         if (i == 70) {
+             continue;
+         }
+          // code <- means code will run which is i == 71;
+
+         if (i == 72) {
+             continue;
+         }
+          // code <- means code will run which is i == 73;
+
+         if (i == 74) {
+             continue;
+         }
+          // code <- means code will run which is i == 75;
+
+         if (i == 76) {
+             continue;
+         }
+          // code <- means code will run which is i == 77;
+
+         if (i == 78) {
+             continue;
+         }
+          // code <- means code will run which is i == 79;
+
+         if (i == 80) {
+             continue;
+         }
+          // code <- means code will run which is i == 80;
+
+         if (i == 82) {
+             continue;
+         }
+          // code <- means code will run which is i == 83;
+
+         if (i == 84) {
+             continue;
+         }
+          // code <- means code will run which is i == 85;  
+
+         if (i == 86) {
+             continue;
+         }
+          // code <- means code will run which is i == 87;
+
+          if (i == 88) {
+             continue;
+         }
+          // code <- means code will run which is i == 89;
+
+          if (i == 90) {
+             continue;
+         }
+          // code <- means code will run which is i == 91;
+
+          if (i == 92) {
+             continue;
+         }
+          // code <- means code will run which is i == 93;
+          if (i == 94) {
+             continue;
+         }
+          // code <- means code will run which is i == 95;
+          if (i == 96) {
+             continue;
+         }
+          // code <- means code will run which is i == 97;
+          if (i == 98) {
+             continue;
+         }
+          // code <- means code will run which is i == 99;
+
+          if (i == 100) {
+             continue;
+         }
+          // more code till 999;
+
+        }
+        
+   }
+
+
+}
+
